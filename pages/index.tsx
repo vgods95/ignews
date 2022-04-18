74300c12fd2d1a017a92d27bfcd1c13c636a2d02
Vinicius Godoy Ferreira <viniciusgodoyf@gmail.com>
2022-04-18 15:53:38 -0300

    Finaliza Fundamentos do Next.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { SubscribeButton } from '../src/components/SubscribeButton';
+import { stripe } from '../src/services/stripe';
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -13,11 +23,34 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl writing" />
       </main>
     </>
   )
+}
+
+//Usar sempre esse nome e ela sempre de forma assíncrona mesmo que não tenha await dentro
+//Só pode ser usado em páginas e não em components
+//export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KoYHQFuoB0Xh0YAK6o4SIuc', { expand: ['product'] });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24 horas (60 segundos * 60 minutos * 24 horas)
+  }
 }
